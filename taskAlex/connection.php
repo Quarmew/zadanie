bdd83112fe5d1934b783436ab526bc4543090686
HeckSteack <uefgoo@gmail.com>
2020-03-22 22:46:19 +0500

    Revert "122"
    This reverts commit 77569da4445068a6f55b23f8bdd531d84d05917a.

--- taskAlex/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'taskalex'; // адрес сервера 
-$database = 'fortaskalex'; // имя базы данных
-$user = 'mysql'; // имя пользователя
-$password = 'mysql'; // пароль  
-?>
--- taskAlex/control_panel.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['user_id'])){ echo "You're autorized.";} else {header('Location: autorization.php');}
-?>    
-</body>
